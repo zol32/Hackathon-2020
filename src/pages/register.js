3af37d7b965e32eb0e681b50e31250f59bd8a4b3
tim-nwn <74809870+tim-nwn@users.noreply.github.com>
2020-11-21 10:46:13 -0500

    Add files via upload

---
+++ src/pages/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class register extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default register
