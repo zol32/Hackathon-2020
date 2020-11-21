88bc9097093237bebc42ae6f748c62c501b372ae
tim-nwn <74809870+tim-nwn@users.noreply.github.com>
2020-11-21 13:22:32 -0500

    Add files via upload

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Fragment } from 'react';
+import SignUp from './SignUp';
+
+// MUI stuff
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
 class register extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
-        )
+            <SignUp />
+        );
     }
 }
 
